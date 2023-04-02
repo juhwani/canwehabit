a34f3336abeb723db1e07e8e376fdd2c5c5a02de
Juhwan Jung <remagman@gmail.com>
2023-04-02 05:51:50 -0400

    Partially Uploading Mypage draft

--- Frontend/my-react-app/src/Components/Modal/Modal.js
+++ Frontend/my-react-app/src/Components/Modal/Modal.js
@@ -3,58 +3,90 @@ import "./Modal.css";
 
 function Modal({ setOpenModal }) {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [goalAccepted, setGoalAccepted] = useState(false);
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
+  };
+
+  const handleDonateClick = () => {
+    setGoalAccepted(true);
   };
 
   return (
     <div className="modalBackground">
       <div className="modalContainer">
         <div className="titleCloseBtn">
+            
           <button
             onClick={() => {
               setOpenModal(false);
             }}
           >
-            
+            X
           </button>
         </div>
-        <div className="title">
+        
+        {goalAccepted ? (
+            <p/>
+        ) : (
           <div className="Charity">
+              <div className="title">
             Which Charity you want to donate (When you fail the goals)
           </div>
         </div>
-        <div className="body">
-          <button
-            className={selectedItem === "Scholarships" ? "two" : "one"}
-            onClick={() => handleItemClick("Scholarships")}
-          >
-            Scholarships
-          </button>
-          <button
-            className={selectedItem === "Make-a-wish Foundation" ? "two" : "one"}
-            onClick={() => handleItemClick("Make-a-wish Foundation")}
-          >
-            Make-a-wish Foundation
-          </button>
-          <button
-            className={selectedItem === "Volunteer Organization" ? "two" : "one"}
-            onClick={() => handleItemClick("Volunteer Organization")}
-          >
-            Volunteer Organization
-          </button>
-        </div>
+        )}
+        {goalAccepted ? (
+          <div className="body"><div className= "inside">Goal Accepted. Good luck!</div></div>
+        ) : (
+          <div className="body">
+            <button
+              className={selectedItem === "Scholarships" ? "two" : "one"}
+              onClick={() => handleItemClick("Scholarships")}
+            >
+              Scholarships
+            </button>
+            <button className={selectedItem === "Make-a-wish Foundation" ? "two" : "one"}
+              onClick={() => handleItemClick("Make-a-wish Foundation")}
+            >Make-a-wish Foundation</button>
+            <button className={selectedItem === "Volunteer Organization" ? "two" : "one"}
+              onClick={() => handleItemClick("Volunteer Organization")}
+            >Volunteer Organization</button>
+            
+          </div>
+        )}
         <div className="footer">
+          {!goalAccepted && (
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+              id="cancelBtn"
+            >
+              Cancel
+            </button>
+          )}
+          {goalAccepted ? (
           <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
+          className="back"
+          onClick={handleDonateClick}
+          onClick={() => {
+            setOpenModal(false);
+          }}
+          
+        >
+          Back
+        </button>
+        ) : (
+            <button
+            className="donate"
+            onClick={handleDonateClick}
+            
+            
           >
-            Cancel
+            Donate
           </button>
-          <button className="donate">Donate</button>
+           )}
         </div>
       </div>
     </div>
--- Frontend/my-react-app/src/pages/mypage/mypage.js
+++ Frontend/my-react-app/src/pages/mypage/mypage.js
@@ -1,9 +1,51 @@
-import React from 'react';
+
 import "./mypage.css"
+import ProgressBar from "@ramonak/react-progress-bar";
 
 function mypage() {
+    const now = 60;
     return(
-        <div>mypage</div>
+        <div className = "MainContainer">
+                
+            <div className = "SubContainer">
+                <div className = "ProfileCon">
+                    <div className = "ProfilePic"></div>
+                    <div className = "NameCon">
+                        <div className = "Name">JANE DOE</div>
+                        <div className = "Points">Total&#128176;: 50</div>
+                    </div>
+                </div>
+
+                <div className = "Bar">
+                    <div>Succeeded</div>
+                    <div>Failed</div>
+                </div>
+                <div className = "Progress">
+                    <ProgressBar bgColor = "#89ACD2" completed="60" width = "1500%" />
+                    </div>
+                <div className = "Columns">
+                    <button className = "choose">In Progress</button>
+                    <button className = "choose">Succeeded</button>
+                    <button className = "choose">Failed</button>
+
+                </div>
+
+                <div className = "choices">
+                    <button className = "select">1</button>
+                    <button className = "select">2</button>
+                    <button className = "select">3</button>
+                    <button className = "select">3</button>
+                    <button className = "select">3</button>
+
+                </div>
+
+                <div className = "Liked"> Challenges you liked</div>
+                
+            </div>
+        
+        
+        
+        </div>
     )
 }
 
