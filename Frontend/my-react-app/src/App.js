f6a1fb005c957da823442b6a112d2e0427b05799
Juhwan Jung <remagman@gmail.com>
2023-04-01 21:30:48 -0400

    first

--- Frontend/my-react-app/src/App.js
+++ Frontend/my-react-app/src/App.js
@@ -5,9 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className= "Bar">
+          Bar
+
+
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
